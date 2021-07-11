c73a5843eb7202b4c802b8c53e56fb43255a8f13
Chandan Rabha <58772198+ChandanRabha@users.noreply.github.com>
2021-07-11 17:55:05 +0530

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Response From Backend on intervals!" });
+});
+app.post("/dynamicinput", (req, res) => {
+  res.json({ sentData: req.body });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
